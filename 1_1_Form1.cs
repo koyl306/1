2b378dc3cd501d5189d124047e8e3887d3ffd82d
koyl306 <e.grigorenko@istu.edu.ua>
2026-03-13 21:18:10 +0200

    Add files via upload

---
+++ 1_1_Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace pract1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsDigit(e.KeyChar) || e.KeyChar == '.' || e.KeyChar == (char)Keys.Back); //шоб тільки цифри, взято зі стаксоверфлоу. змінила, тепер шоб тільки цифри крапка і бекспейс
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //litres
+            double gotlitres = 0;
+            
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Скіки залили????");
+                textBox1.Focus();
+            }else
+                gotlitres = double.Parse(textBox1.Text);
+
+            //gas type
+            double price = 0;
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case -1:
+                    MessageBox.Show("Який бензин?????");
+                    break;
+
+                case 0:
+                    //92
+                    price = 63.78;
+                    break;
+                
+                case 1:
+                    //95
+                    price = 67.2;
+                    break;
+
+                case 2:
+                    //98
+                    price = 71.67;
+                    break;
+                
+                default:
+                    price = 1;
+                    break;
+            }
+
+            //discount
+            string discountText = textBox2.Text;
+            bool discountValid = false;
+            if (discountText == "FF35")
+                discountValid = true;
+
+            //calc(short from calculator chat)
+            double fullprice=0;
+            if (gotlitres>0 && price > 0)
+            {
+                fullprice = gotlitres * price;
+                if (discountValid)
+                    fullprice = fullprice * 0.35;
+            }
+            
+            textBox3.Text = Convert.ToString(fullprice);
+        }
+    }
+}
---
+++ 1_2_Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pract1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsDigit(e.KeyChar) ||  e.KeyChar == (char)Keys.Back); //взято зі стаксоверфлоу
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            //name and surname
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Введіть ім'я");
+                textBox1.Focus();
+                return;
+            }
+            string name = Convert.ToString(char.ToUpper(textBox1.Text[0]) + textBox1.Text.Substring(1));
+
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Введіть прізвище");
+                textBox2.Focus();
+                return;
+            }
+            string surname = Convert.ToString(char.ToUpper(textBox2.Text[0]) + textBox2.Text.Substring(1));
+
+            //year
+            int year =0;
+            int tb4=0;
+            if (string.IsNullOrEmpty(textBox3.Text))
+            {
+                MessageBox.Show("Введіть рік народження");
+                textBox3.Focus();
+                return;
+            } else
+                tb4 = Convert.ToInt32(textBox3.Text);
+
+            if (tb4<=2012 && tb4>=1900)
+                year = tb4;
+            else
+            {
+                MessageBox.Show("Введіть СПРАВЖНІЙ рік народження");
+                textBox3.Focus();
+                return;
+            }
+
+            //group
+            string group = "";
+            if (string.IsNullOrEmpty(textBox4.Text))
+            {
+                MessageBox.Show("Введіть групу");
+                textBox4.Focus();
+                return;
+            }
+            group = textBox4.Text;
+
+            //course
+            int course = 0;
+            switch (comboBox1.SelectedIndex)
+            {
+                case -1:
+                    MessageBox.Show("Оберіть курс");
+                    return;
+                    break;
+
+                case 0:
+                    course = 1;
+                    break;
+
+                case 1:
+                    course = 2;
+                    break;
+
+                case 2:
+                    course = 3;
+                    break;
+
+                case 3:
+                    course = 4;
+                    break;
+
+                case 4:
+                    course = 5;
+                    break;
+            }
+
+            listBox1.Items.Add(name);
+            listBox1.Items.Add(surname);
+            listBox1.Items.Add(year);
+            listBox1.Items.Add("група " + group);
+            listBox1.Items.Add(course + " курс");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
